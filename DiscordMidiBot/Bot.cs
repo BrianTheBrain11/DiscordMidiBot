2d7ece303b4c557da5e533da972b0f5413cc201e
BrianDaBrain11 <creeperman1234@yahoo.com>
2021-04-06 16:49:40 -0700

    Add project files.

---
+++ DiscordMidiBot/Bot.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordMidiBot
+{
+    public class Bot
+	{
+		public DiscordClient client { get; private set; }
+		public CommandsNextModule commands { get; private set; }
+
+		public async Task RunAsync()
+		{
+			string json = string.Empty;
+
+			using (FileStream fs = File.OpenRead("config.json"))
+			{
+				using (StreamReader sr = new StreamReader(fs, new UTF8Encoding(false)))
+				{
+					json = await sr.ReadToEndAsync().ConfigureAwait(false);
+				}
+			}
+
+			var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+			DiscordConfiguration config = new DiscordConfiguration
+			{
+				Token = configJson.Token,
+				TokenType = TokenType.Bot,
+				AutoReconnect = true,
+				LogLevel = LogLevel.Debug,
+				UseInternalLogHandler = true
+			};
+
+			client = new DiscordClient(config);
+
+			client.Ready += OnClientReady;
+
+			CommandsNextConfiguration commandsConfig = new CommandsNextConfiguration
+			{
+				StringPrefix = configJson.Prefix,
+				EnableDms = true,
+				EnableMentionPrefix = true,
+			};
+
+			commands = client.UseCommandsNext(commandsConfig);
+
+			commands.RegisterCommands<Commands>();
+
+			await client.ConnectAsync();
+
+			await Task.Delay(-1);
+		}
+
+		private Task OnClientReady(ReadyEventArgs e)
+        {
+			return Task.CompletedTask;
+        }
+	}
+}
---
+++ DiscordMidiBot/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using DSharpPlus;
+using System.Threading;
+using System.Threading.Tasks;
+using Melanchall.DryWetMidi.Devices;
+using Melanchall.DryWetMidi.Core;
+
+namespace DiscordMidiBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Program Begun: DisocrdMidiBot");
+
+			Thread botThread = new Thread(BotInitialize.StartBot);
+			botThread.Start();
+
+			Thread midiThread = new Thread(MidiInitialize.StartMidi);
+			midiThread.Start();
+		}
+	}
+
+    public class BotInitialize
+    {
+		public static void StartBot()
+        {
+			Bot bot = new Bot();
+			bot.RunAsync().GetAwaiter().GetResult();
+			Console.ReadKey();
+        }
+    }
+
+	public class MidiInitialize
+    {
+		public static void StartMidi()
+        {
+
+        }
+
+		public static void MidiLoop()
+        {
+            using (var outputDevice = OutputDevice.GetByName("MIDI Device"))
+            {
+                outputDevice.EventSent += OnEventSent;
+
+                using (var inputDevice = InputDevice.GetByName("MIDI Device"))
+                {
+                    inputDevice.EventReceived += OnEventReceived;
+                    inputDevice.StartEventsListening();
+
+                    outputDevice.SendEvent(new NoteOnEvent());
+                    outputDevice.SendEvent(new NoteOffEvent());
+                }
+            }
+        }
+
+        private static void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
+        {
+            var midiDevice = (MidiDevice)sender;
+            Console.WriteLine($"Event received from '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
+        }
+
+        private static void OnEventSent(object sender, MidiEventSentEventArgs e)
+        {
+            var midiDevice = (MidiDevice)sender;
+            Console.WriteLine($"Event sent to '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
+        }
+    }
+}
